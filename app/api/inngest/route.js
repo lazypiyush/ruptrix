69fce4ae491ded71d49d84ea186a1368e2ec128c
lazypiyush <piyushmandolikar@gmail.com>
2025-06-15 02:10:08 +0530

    Delete app/api/inngest directory

--- app/api/inngest/route.js
+++
@@ -1,8 +0,0 @@
-import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
-import { serve } from "inngest/next";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [checkBudgetAlerts, triggerRecurringTransactions, processRecurringTransaction, generateMonthlyReports],
-});
