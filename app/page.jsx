770c1c585d5362d25cc5f1a2e23afced091b5e8f
lazypiyush <piyushmandolikar@gmail.com>
2025-06-16 16:18:25 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,9 +13,10 @@ export default function Home() {
 
       <section className="py-0">
         <div className="container mx-auto px-4">
-          <h2 className="text-5xl font-bold text-center mb-12">
-              Everything you need to manage your finances
-          </h2>
+          <h2 className="text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-center mb-12">
+  Everything you need to manage your finances
+</h2>
+
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {featuresData.map((feature, index) => (
