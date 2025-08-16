21a876196851fce68453ebeeaba6324ecb850ead
lazypiyush <piyushmandolikar@gmail.com>
2025-08-17 01:10:45 +0530

    Update layout.js

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,15 +1,25 @@
 import DashboardPage from "./page";
 import { BarLoader } from "react-spinners";
 import { Suspense } from "react";
+import Script from "next/script";
 
 export default function Layout() {
   return (
     <div className="px-5">
+      {/* Google AdSense script */}
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
       <div className="flex items-center justify-between mb-5">
         <h1 className="text-6xl font-bold tracking-tight gradient-title">
           Dashboard
         </h1>
       </div>
+
       <Suspense
         fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
