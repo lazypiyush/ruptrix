dd5dcc1bc82bf769e3d7bc40d4b46aa8ce7d6f16
lazypiyush <piyushmandolikar@gmail.com>
2025-08-17 01:10:05 +0530

    Update layout.js

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Script from 'next/script';
 
-const MainLayout = ({children}) => {
-  return <div className="container mx-auto my-32">{children}</div>
+const MainLayout = ({ children }) => {
+  return (
+    <div className="container mx-auto my-32">
+      {/* Google AdSense script */}
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
+      {children}
+    </div>
+  );
 };
 
-export default MainLayout
+export default MainLayout;
